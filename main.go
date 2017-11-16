d7c8efc314b50b512a2a8b11aa89ae7ddcd58cf3
Michael Aldridge <aldridge.mac@gmail.com>
2017-11-15 23:20:21 -0800

    Find and create a list of videos

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"log"
-	"fmt"
 	"flag"
-	"net/http"
+	"fmt"
 	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
 )
 
 var (
 	templates = template.Must(template.ParseFiles("tmpl/status.tmpl"))
 
-	port = flag.Int("port", 8080, "Serving port")
+	port     = flag.Int("port", 8080, "Serving port")
+	videoDir = flag.String("video_dir", "video", "Directory to search for files to be tagged")
+
+	videos []string
 )
 
 func OKHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,21 +24,51 @@ func OKHandler(w http.ResponseWriter, r *http.Request) {
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
 	s := struct {
-		Port int
+		Port     int
+		VideoDir string
+		Videos   []string
 	}{
-		Port: *port,
+		Port:     *port,
+		VideoDir: *videoDir,
+		Videos:   videos,
 	}
-		
-	
+
 	err := templates.ExecuteTemplate(w, "status.tmpl", s)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
+func findVideos() {
+	log.Println("Begining video search")
+	path, err := filepath.Abs(*videoDir)
+	if err != nil {
+		log.Fatalf("Error getting video path: %s", err)
+	}
+	*videoDir = path
+	log.Printf("Loading videos from %s", *videoDir)
+
+	videos, err = filepath.Glob(path + "/*")
+	if err != nil {
+		log.Printf("Error globbing videos: %s", err)
+	}
+
+	log.Println("Located the following files:")
+	for i, v := range videos {
+		v = filepath.Base(v)
+		videos[i] = v
+		log.Printf("  %s", v)
+	}
+}
+
 func main() {
+	flag.Parse()
 	log.Println("Tagr Server is initializing...")
+
 	http.HandleFunc("/ok", OKHandler)
 	http.HandleFunc("/status", statusHandler)
+
+	findVideos()
+
 	http.ListenAndServe(":8080", nil)
 }
